dcff6a1b49827ea282395504f7c8b422b67c8ace
jon <joao.couto@mindswap.academy>
2022-06-23 19:12:09 +0100

    branch updates controller + service

--- src/main/java/europcar/project/controllers/UserController.java
+++ src/main/java/europcar/project/controllers/UserController.java
@@ -2,11 +2,12 @@ package europcar.project.controllers;
 
 
 import europcar.project.command.UserDto;
+import europcar.project.persistence.models.User;
 import europcar.project.services.UserServiceI;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,17 @@ public class UserController {
     @GetMapping
     public List<UserDto> getUsers() {
         return this.userServiceI.getUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> signUp(@RequestBody UserDto userDto){
+        UserDto responseDto = this.userServiceI.signUp(userDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "{UserId}")
+    public ResponseEntity<UserDto> deleteUser(@PathVariable("UserId") Long userId){
+        UserDto userDeleted = this.userServiceI.deleteUser(userId);
+        return new ResponseEntity<>(userDeleted, HttpStatus.OK);
     }
 }
--- src/main/java/europcar/project/services/UserServiceImp.java
+++ src/main/java/europcar/project/services/UserServiceImp.java
@@ -1,18 +1,44 @@
 package europcar.project.services;
 
 import europcar.project.command.UserDto;
+import europcar.project.converters.UserConverterImp;
+import europcar.project.persistence.models.User;
 import europcar.project.persistence.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImp {
+public class UserServiceImp implements UserServiceI{
     private UserRepository userRepository;
+    private UserConverterImp userConverterImp;
 
-//    public List<UserDto> getStudents() {
-//        return this.studentConverter.entityListToDtoList(this.repository.findAll());
-//    }
+
+    @Override
+    public List<UserDto> getUsers() {
+        return this.userConverterImp.convertEntityListToDtoList(this.userRepository.findAll());
+    }
+
+    @Override
+    public UserDto signUp(UserDto userDto) {
+        User userToSave = userConverterImp.dtoToEntity(userDto);
+
+        this.userRepository.save(userToSave);
+
+        return userConverterImp.entityToDto(userToSave);
+    }
+
+    @Override
+    public UserDto deleteUser(Long userId) {
+        User userToDelete = userRepository.findById(userId).orElse(null);
+
+        this.userRepository.deleteById(userId);
+
+        return userConverterImp.entityToDto(userToDelete);
+    }
+
+
 }
