363a1c5a1210ab9b21da886bdd75da64bfd05618
jon <joao.couto@mindswap.academy>
2022-06-24 13:01:25 +0100

    post with full list

--- src/main/java/europcar/project/controllers/UserController.java
+++ src/main/java/europcar/project/controllers/UserController.java
@@ -4,6 +4,7 @@ package europcar.project.controllers;
 import europcar.project.command.UserDto;
 import europcar.project.command.UserUpdateDto;
 import europcar.project.persistence.models.User;
+import europcar.project.persistence.repositories.Populate;
 import europcar.project.services.UserServiceI;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,19 +25,36 @@ public class UserController {
         return this.userServiceI.getUsers();
     }
 
+    @GetMapping(path = "/byid/{UserId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("UserId") Long userId){
+        UserDto userDeleted = this.userServiceI.getUserById(userId);
+        return new ResponseEntity<>(userDeleted, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/byname/{UserName}")
+    public List<UserDto> getUserByName(@PathVariable("UserName") String userName){
+        return this.userServiceI.getUserByName(userName);
+    }
+
+    @PostMapping(path = "/signupall/")
+    public ResponseEntity<List<UserDto>> signUpAll(@Valid @RequestBody Populate<UserDto> usersList){
+        List<UserDto> responseDtoList = this.userServiceI.signUpAll(usersList);
+        return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<UserDto> signUp(@Valid @RequestBody UserDto userDto){
         UserDto responseDto = this.userServiceI.signUp(userDto);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-    @DeleteMapping(path = "{UserId}")
+    @DeleteMapping(path = "/{UserId}")
     public ResponseEntity<UserDto> deleteUser(@PathVariable("UserId") Long userId){
         UserDto userDeleted = this.userServiceI.deleteUser(userId);
         return new ResponseEntity<>(userDeleted, HttpStatus.OK);
     }
 
-    @PutMapping(path = "{UserID}")
+    @PutMapping(path = "/{UserID}")
     public ResponseEntity<UserDto> updateStudent(@PathVariable("UserID") Long id, @Valid @RequestBody UserUpdateDto userUpdateDto) {
         UserDto userUpdated = this.userServiceI.updateUser(id, userUpdateDto);
         return new ResponseEntity<>(userUpdated, HttpStatus.OK);
--- src/main/java/europcar/project/persistence/repositories/UserRepository.java
+++ src/main/java/europcar/project/persistence/repositories/UserRepository.java
@@ -2,6 +2,15 @@ package europcar.project.persistence.repositories;
 
 import europcar.project.persistence.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    //@Query(value = "FROM User WHERE name = ?1")
+    List<User> findByName(String name);
 }
